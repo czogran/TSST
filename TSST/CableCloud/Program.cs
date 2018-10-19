c7b015e63b950583d65788827d26716649200160
pgaska <pawegska@ymail.com>
2018-10-19 14:30:59 +0200

    szkielet kodu

---
+++ TSST/CableCloud/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CableCloud
+{
+    /// <summary>
+    /// Główna klasa programu
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args">Nieużywane</param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TSST/NetworkNode/Port.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkNode
+{
+    /// <summary>
+    /// Port
+    /// </summary>
+    class Port
+    {
+    }
+}
