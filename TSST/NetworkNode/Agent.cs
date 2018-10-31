9bfbcfa6b6758be5395e3b3cb55eaad2a4bcf547
pgaska <pawegska@ymail.com>
2018-10-31 17:53:52 +0100

    dodanie socketu managera, socketu agenta, współbierzność network node

--- TSST/NetworkNode/Agent.cs
+++ TSST/NetworkNode/Agent.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetworkNode
@@ -11,5 +14,140 @@ namespace NetworkNode
     /// </summary>
     class Agent
     {
+        /// <summary>
+        /// Sockecik
+        /// </summary>
+        public Socket socket;
+
+        /// <summary>
+        /// Adres końca portu
+        /// </summary>
+        public IPEndPoint ipEndPoint;
+        
+        public string test;    ///do usuniecia
+
+        /// <summary>
+        /// Adres portu
+        /// </summary>
+        private string address;
+
+        /// <summary>
+        /// Nr portu
+        /// </summary>
+        private int port;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="address">adres portu</param>
+        /// <param name="port">Nr portu</param>
+        public Agent(string address, int port)
+        {
+            this.address = address;
+            this.port = port;
+            ipEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+        }
+        
+        /// <summary>
+        /// Głowna pętla agenta. Trzeba tak zrobić bo wątek potrzebuje delegata tej metody
+        /// </summary>
+        public void Execute()
+        {
+            while (true)
+            {
+                Listen();
+                SendData("127.0.0.30", "czesc");
+            }
+        }
+
+        /// <summary>
+        /// Wysyła strumień bitów
+        /// </summary>
+        /// <param name="receiver">adres na który wysłane będą dane</param>
+        /// <param name="data">dane do wysłania</param>
+        public void SendData(string receiver, string data)
+        {
+            try
+            {
+                CreateSocket();
+
+                socket.Connect(receiver, port);
+                socket.Send(ASCIIEncoding.ASCII.GetBytes(data));
+                Console.WriteLine("SENDING");
+
+                Close();
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.ToString());
+            }
+        }
+        
+        /// <summary>
+        /// Nasłuchuje czy przychodzą dane
+        /// </summary>
+        public void Listen()
+        {
+            try
+            {
+                socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.IP);
+                socket.Bind(ipEndPoint);
+                
+                socket.Listen(1);
+                Console.WriteLine("LISTENING");
+
+                Socket handler = socket.Accept();
+                string result = ReceiveData(handler);
+                test = result+"0";
+                Console.WriteLine(result);
+                
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        
+        /// <summary>
+        /// Zwraca otrzymane dane
+        /// </summary>
+        /// <returns>odebrany string</returns>
+        private string ReceiveData(Socket handler)
+        {
+            string result ="";
+            int bytes = 0;
+            Byte[] buffer = new Byte[256];
+
+            bytes = handler.Receive(buffer, buffer.Length, 0);
+            result = Encoding.ASCII.GetString(buffer, 0, bytes);
+            
+            return result;
+        }
+
+        /// <summary>
+        /// Tworzy socketa
+        /// </summary>
+        private void CreateSocket()
+        {
+            socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.IP);
+        }
+
+        /// <summary>
+        /// Zamyka socketa
+        /// </summary>
+        private void Close()
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+                
+        /// <summary>
+        /// Jakiś handler pewnie się przyda
+        /// </summary>
+        private void Handle()
+        {
+            
+        }
     }
 }
--- TSST/NetworkNode/Program.cs
+++ TSST/NetworkNode/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetworkNode
@@ -19,14 +21,14 @@ namespace NetworkNode
         static void Main(string[] args)
         {
             Port port = new Port("127.0.0.4", 10000);
-            port.Listen();
-            port.SendData("127.0.0.1", port.test);
-            
-            //tu będzie piękna pętla w której będzie chodził program
-//            while (true)
-//            {
-//                
-//            }
+            Agent agent = new Agent("127.0.0.10", 10000);
+            //testy
+            Thread thread1 = new Thread(new ThreadStart(port.Execute));
+            Thread thread2 = new Thread(new ThreadStart(agent.Execute));
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
         }
     }
 }
