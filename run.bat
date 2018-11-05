37ef8ed941a4dcf464ffdba4d4a8f0d1b03a279a
Karol Musolff <karolmusolff@gmail.com>
2018-11-05 18:37:57 +0100

    wersja beta batcha, uruchamia wezly sieciowe i chmure kablowa

---
+++ run.bat
@@ -0,0 +1,31 @@
+@echo off
+color A
+
+echo Sprawdzanie uprawnien uzytkownika
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	echo Dostateczne uprawnienia zapewnione
+) else (
+    echo Blad. Uruchom program jako administrator
+    exit
+)
+
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+set /p ile_wezlow="Ile wezlow sieciowych: "
+
+echo Tworze %ile_wezlow% wezlow
+
+for /l %%x in (1, 1, %ile_wezlow%) do start /d "TSST\NetworkNode\bin\Debug" NetworkNode.exe
+
+echo Uruchamiam chmure kablowa
+
+start /d "TSST\CableCloud\bin\Debug" CableCloud.exe
+
+
+
+
+
+
