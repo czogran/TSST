598937b7f282bc29b80ba556647187d22941821a
Karol Musolff <karolmusolff@gmail.com>
2018-12-05 18:33:22 +0100

    CLI

--- TSST/ClientNode/CLI.cs
+++ TSST/ClientNode/CLI.cs
@@ -11,6 +11,56 @@ namespace ClientNode
     /// </summary>
     class CLI
     {
-        
+        /// <summary>
+        /// Wysłanie managerowi potwierdzenia, że zrobiło się wszystko tak jak chciał
+        /// </summary>
+        public static readonly string confirmation = "Zrobiłem wszystko tak jak chciałeś i wysyłam potwierdzenie";
+
+        /// <summary>
+        /// Wysłanie managerowi errora jak nie zadziała
+        /// </summary>
+        public static readonly string managerError = "Nie udało mi się zrobić wszystkiego tak jak chciałeś i wysyłam errora";
+
+        /// <summary>
+        /// Wypisuje błąd podczas wysyłania
+        /// </summary>
+        public static void PrintError()
+        {
+            Console.WriteLine("Błąd podczas wysyłania");
+        }
+
+        /// <summary>
+        /// Printuje treść otrzymanej wiadomości
+        /// </summary>
+        /// <param name="message">treść wiadomości</param>
+        /// <param name="port">numer portu</param>
+        public static void PrintReceivedMessage(string message)
+        {
+            Console.WriteLine($"{DateTime.Now} {message}");
+        }
+
+        /// <summary>
+        /// Printuje wysłanie wiadomości
+        /// </summary>
+        /// <param name="message">treść wiadomości</param>
+        /// <param name="port">numer portu</param>
+        public static void PrintSentMessage(string message)
+        {
+            Console.WriteLine($"{DateTime.Now} Wysłano {message}");
+        }
+
+        /// <summary>
+        /// Informacja o odebraniu XML od managera
+        /// </summary>
+        /// <param name="name"></param>
+        public static void PrintReceivedXML(string name)
+        {
+            Console.WriteLine($"{DateTime.Now} Otrzymano plik XML: {name}");
+        }
+
+        public static void PrintException(Exception e)
+        {
+            Console.WriteLine($"{DateTime.Now} Zlapany wyjatek:\n {e.ToString()}" );
+        }
     }
 }
